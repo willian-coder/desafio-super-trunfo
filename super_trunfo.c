b7df2120a5fb0372bafb913a023f978f6f7aa057
Willianlq <68751621+willian-coder@users.noreply.github.com>
2025-06-17 22:04:48 +0000

    desafio nivel aventureiro

--- super_trunfo.c
+++ super_trunfo.c
@@ -9,6 +9,8 @@ float area1, pib1 = 0.00;
 char estado1[10];
 char codcarta1[8];
 char nomeCidade1[45];
+float  calcDensidadePop1; // variavel densidade
+float calcPIB1; // variavei de calculo pib
 
 //Carta numero 1
 printf("Escolha sua primeira carta \n");
@@ -51,6 +53,8 @@ float area2, pib2 = 0.00;
 char estado2[10];
 char codcarta2[8];
 char nomeCidade2[45];
+float calcDensidadePop2;// variavel densidade
+float calcPIB2;// variavei de calculo pib
 
 //Carta numero 2
 printf("\n\nEscolha sua segunda carta \n");
@@ -95,15 +99,31 @@ printf("Área: %.2f\n", area1);//limitado a duas casas
 printf("PIB: %.2f\n", pib1);//limitado a duas casas
 printf("Numero de pontos turisticos: %d\n", numPontosTuristicos1);
 
+//Calculo densidade populacional
+calcDensidadePop1 = (float) populacao1 / area1;
+printf("Densidade Populacional: %.2f hab/km²\n", calcDensidadePop1);
+
+//Pib / população
+calcPIB1 = (float) pib1 / populacao1;
+printf("PIB per Capita: %.2f reais\n", calcPIB1);
+
 //Exibição segunda carta
 printf("\nCarta 2:\n");
 printf("Estado: %s\n", estado2);
 printf("Código: %s\n", codcarta2);
 printf("Nome da Cidade: %s\n", nomeCidade2);
 printf("População: %d\n", populacao2);
-printf("Área: %.2f\n", area2);//limitado a duas casas
+printf("Área: %.2f km²\n", area2);//limitado a duas casas
 printf("PIB: %.2f\n", pib2);//limitado a duas casas
 printf("Numero de pontos turisticos: %d\n", numPontosTuristicos2);
+
+//Calculo densidade populacional
+calcDensidadePop2 = (float) populacao2 / area2;
+printf("Densidade Populacional: %.2f hab/km²\n", calcDensidadePop2);
+//Pib / população
+calcPIB2 = (float) pib2 / populacao2;
+printf("PIB per Capita: %.2f reais\n", calcPIB2);
+
 
 //finalizado
 return 0;
